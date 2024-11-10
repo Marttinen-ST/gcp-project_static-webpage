b94f9521dd49b154773fd460f9e5b97266dfb2c4
mt-albert <albert.marttinen@metaltoad.com>
2024-11-10 19:15:36 -0300

    adding google iam binding

--- terraform/modules/static-website-bucket/static-website-bucket.tf
+++ terraform/modules/static-website-bucket/static-website-bucket.tf
@@ -26,3 +26,11 @@ resource "google_storage_bucket_object" "errorpage" {
   content_type = "text/html"
   bucket       = google_storage_bucket.static_website.id
 }
+
+resource "google_storage_bucket_iam_binding" "public_access" {
+  bucket = google_storage_bucket.static_website.name
+  role   = "roles/storage.objectViewer"
+  members = [
+    "allUsers",
+  ]
+}

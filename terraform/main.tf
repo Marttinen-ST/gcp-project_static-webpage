1ceeb3b1a63df7cd81de6c58156bd256af12d712
mt-albert <albert.marttinen@metaltoad.com>
2024-11-10 16:32:52 -0300

    creating terraform backend file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,15 @@
 provider "google" {
-    project = var.project_id
-    region = var.gcp_region
+  project = var.project_id
+  region  = var.gcp_region
+}
+
+resource "google_storage_bucket" "tfstate-bucket" {
+  name          = var.tfstate_bucket
+  location      = var.gcp_region
+  force_destroy = true
+  storage_class = "STANDARD"
+  versioning {
+    enabled = true
+  }
+  uniform_bucket_level_access = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,17 @@
 variable "project_id" {
   description = "Google Prject ID"
-  type = string
-  default = "long-sum-441213-v5"
+  type        = string
+  default     = "long-sum-441213-v5"
 }
 
 variable "gcp_region" {
-    description = "Region to where terraform will deploy to"
-    type = string
-    default = "us-central1"
+  description = "Region to where terraform will deploy to"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "tfstate_bucket" {
+  description = "Bucket name that will store the tfstate file"
+  type        = string
+  default     = "test-env-tfstate"
 }
